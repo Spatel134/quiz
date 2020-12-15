349ec7f0cd0379c01a750e0edde954ac11f014fb
Sahil Patel <patelsaho@gmail.com>
2020-12-15 18:56:59 -0500

    Format highscores page and buttons

--- Assets/score.js
+++ Assets/score.js
@@ -6,50 +6,55 @@ var clearHighScores = document.querySelector("#clear-highscores");
 forTheScores();
 
 function forTheScores() {
-  storedScores = JSON.parse(localStorage.getItem("scores"));
+    storedScores = JSON.parse(localStorage.getItem("scores"));
 
-  if (storedScores !== null) {
-    scoreList = storedScores;
-  }
-  createScores();
+    if (storedScores !== null) {
+        scoreList = storedScores;
+    }
+    createScores();
 }
 
 function clearAll() {
-  window.localStorage.clear();
+    window.localStorage.clear();
 }
 
 function createScores() {
-  if (storedScores !== null) {
-    scoreList.sort(function (a, b) {
-      return a.newScore - b.newScore;
-    });
-    scoreList.reverse(function (a, b) {
-      return a.newScore - b.newScore;
-    });
+    if (storedScores !== null) {
+        scoreList.sort(function (a, b) {
+            return a.newScore - b.newScore;
+        });
+        scoreList.reverse(function (a, b) {
+            return a.newScore - b.newScore;
+        });
 
-    for (i = 0; i < scoreList.length; i++) {
-      var scoreListItem = scoreList[i];
-      var tr = document.createElement("tr");
-      var nameCell = document.createElement("td");
-      var nameCellText = document.createTextNode(scoreListItem.name);
-      var scoreCell = document.createElement("td");
-      var scoreCellNum = document.createTextNode(scoreListItem.newScore);
+        for (i = 0; i < scoreList.length; i++) {
+            var scoreListItem = scoreList[i];
+            var tr = document.createElement("tr");
 
-      tr.setAttribute("tr-index", i);
-      document.getElementById("highScores").appendChild(tr);
-      tr.appendChild(nameCell);
-      nameCell.appendChild(nameCellText);
-      tr.appendChild(scoreCell);
-      scoreCell.appendChild(scoreCellNum);
+            var positionCell = document.createElement("th");
+            positionCell.setAttribute("scope", "row")
+            positionCell.append(document.createTextNode(i + 1));
+
+            var nameCell = document.createElement("td");
+            nameCell.append(document.createTextNode(scoreListItem.name));
+
+            var scoreCell = document.createElement("td");
+            scoreCell.append(document.createTextNode(scoreListItem.newScore));
+
+            tr.setAttribute("tr-index", i);
+            document.getElementById("highScores").appendChild(tr);
+            tr.appendChild(positionCell);
+            tr.appendChild(nameCell);
+            tr.appendChild(scoreCell);
+        }
     }
-  }
 }
 
 clearHighScores.addEventListener("click", function () {
-  window.location.href = "./highScore.html";
-  clearAll();
+    window.location.href = "./highScore.html";
+    clearAll();
 });
 
 backButton.addEventListener("click", function () {
-  window.location.href = "./index.html";
+    window.location.href = "./index.html";
 });
